fcc60cc5323143c082a165de82d2b41f5a62a5d5
sudoXpg <sudoxpg@gmail.com>
2024-09-20 23:21:23 +0530

    32-bit logical ops added

---
+++ or32bit.v
@@ -0,0 +1,3 @@
+module or32bit(input [31:0]a,b, output [31:0]result);
+assign result = a | b;
+endmodule
---
+++ xor32bit.v
@@ -0,0 +1,3 @@
+module xor32bit(input [31:0]a,b, output [31:0]result);
+assign result = a ^ b;
+endmodule
